013f0b4dd4b85d5e1a4d99ea756d5f656826ab95
dariobaron <barone.dario97@gmail.com>
2024-04-17 13:54:06 +0200

    fixed small bug in parametermanager

--- TravelAndMutate/paramsmanager.py
+++ TravelAndMutate/paramsmanager.py
@@ -3,19 +3,29 @@ import h5py
 
 class Params:
 	def __init__(self, params_dict, rng):
+		# dummy inits
+		self.N_patches = 0
+		self.Ns = []
+		# general parameters
 		self.params_dict = params_dict
 		self.rng = rng
+		# single-valued parameters
 		updater = {key:val for key,val in params_dict.items() if "_setter" not in key and "_params" not in key}
 		self.__dict__.update(updater)
+		# multi-valued parameters
 		setter_parameters = {key.replace("_setter",""):val for key,val in params_dict.items() if "_setter" in key}
 		params_parameters = {key.replace("_params",""):val for key,val in params_dict.items() if "_params" in key}
 		if list(setter_parameters.keys()) != list(params_parameters.keys()):
 			raise ValueError(f"Parameters types and params mismatch:\n{list(setter_parameters.keys())}\nvs\n{list(params_parameters.keys())}")
+		### manually imposing precedence on dependent parameters
 		updater = {}
 		try:
 			updater["Ns"] = getattr(self, setter_parameters["Ns"])(*params_parameters["Ns"])
 		except:
 			updater["Ns"] = getattr(self, setter_parameters["Ns"])(params_parameters["Ns"])
+		self.__dict__.update(updater)
+		### updating all the other parameters
+		updater = {}
 		for key,val in setter_parameters.items():
 			if key == "Ns":
 				continue
@@ -45,8 +55,8 @@ class Params:
 			values = inputfile[pathtodataset]
 		return values
 
-	def gravity(self, scale, alpha, gamma, r, *positions):
-		pos = np.array(positions).reshape((self.N_patches,2))
+	def gravity(self, scale, alpha, gamma, r, file_of_position):
+		pos = self.fromtxt(file_of_position)
 		c_ij = np.empty((self.N_patches,self.N_patches))
 		for i in range(self.N_patches):
 			for j in range(self.N_patches):
