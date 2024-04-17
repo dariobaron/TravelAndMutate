e99d83d5f8c69f6ac531efe0a014e0713e45cf61
dariobaron <barone.dario97@gmail.com>
2024-04-17 10:35:40 +0200

    minor changes

--- TravelAndMutate/datamanager.py
+++ TravelAndMutate/datamanager.py
@@ -4,6 +4,7 @@ import h5py
 import os
 from os.path import isfile
 from tqdm import tqdm
+import glob
 
 
 def filter_groupmembers_with_params(group, params_dict):
@@ -63,3 +64,31 @@ def collect_attribute_from_group(key, group, applyfunc=None):
 	return pd.Series(attributes)
 
 
+def consolidate_h5(inputfilename, outputfilename, suppress_output=False):
+	infilenames = sorted(glob.glob(inputfilename+f"_*_seed-*.h5"))
+	groupnames = [f.split("_")[-2] for f in infilenames]
+	outfilename = outputfilename + ".h5"
+	overwritten = []
+	with h5py.File(outfilename, "a") as outfile:
+		for i,infilename in enumerate(tqdm(infilenames, ncols=80, leave=False)):
+			with h5py.File(infilename) as infile:
+				ingroup = infile.require_group(groupnames[i])
+				params_dict = dict(ingroup.attrs)
+				groupname = filter_groupmembers_with_params(outfile, params_dict)
+				if groupname is None:
+					infile.copy(infile[groupnames[i]], outfile, name=groupnames[i], expand_soft=True, expand_external=True, expand_refs=True)
+				elif groupname == groupnames[i]:
+					outgroup = outfile.require_group(groupname)
+					if len(ingroup.attrs) != len(outgroup.attrs):
+						raise RuntimeError("Number of parameters of the simulation do not match with the ones for the existing group")
+					datasetname = list(ingroup.keys())[0]
+					if datasetname in outgroup:
+						overwritten.append(datasetname)
+						del outgroup[datasetname]
+					infile.copy(ingroup[datasetname], outgroup, name=datasetname, expand_soft=True, expand_external=True, expand_refs=True)
+				else:
+					raise RuntimeError(f"Group-name mismatch ({groupname} vs {groupnames[i]}) for params: {params_dict}")
+	for f in infilenames:
+		os.remove(f)
+	if len(overwritten) > 0:
+		print(f"WARNING: overwritten {len(overwritten)} datasets!")
