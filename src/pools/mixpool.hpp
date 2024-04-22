1be19a1933ad43fd048bf55505fc8a7981b270f2
dariobaron <barone.dario97@gmail.com>
2024-04-22 15:08:29 +0200

    added gammatrick for System<Mix>

--- src/pools/mixpool.hpp
+++ src/pools/mixpool.hpp
@@ -7,59 +7,86 @@
 class MixPool{
 protected:
 	PatchID patch_id_;
-	unsigned size_;
+	double rate_;
+	Vec<unsigned> sizes_;
 public:
 	MixPool(PatchID patch_id, unsigned n=0);
+	MixPool(PatchID patch_id, unsigned gamma_trick, double rate);
 	unsigned size() const;
 	double getPhi() const;
 	MixPool& operator+=(const MixPool & other);
 	MixPool generate(Time, unsigned n);
 	MixPool generate(Time, const MixPool & other);
-	MixPool sample(RNGcore *, unsigned n) const;
-	MixPool sampleWithReplacement(RNGcore *, unsigned n) const;
+	void shift(RNGcore * rng);
+	MixPool getNewErased(RNGcore *) const;
+	MixPool sampleInfectors(RNGcore *, unsigned n) const;
 	void moveFromTo(MixPool & source, MixPool & target) const;
 	void clear();
 };
 
-MixPool::MixPool(PatchID patch_id, unsigned n) : patch_id_(patch_id), size_(n){}
+MixPool::MixPool(PatchID patch_id, unsigned n) : patch_id_(patch_id), sizes_(1,n) {}
+
+MixPool::MixPool(PatchID patch_id, unsigned gamma_trick, double rate) : patch_id_(patch_id), rate_(rate), sizes_(gamma_trick) {}
 
 unsigned MixPool::size() const{
-	return size_;
+	unsigned size = 0;
+	for (auto s : sizes_){
+		size += s;
+	}
+	return size;
 }
 
 double MixPool::getPhi() const{
-	return size_;
+	double phi = 0;
+	for (auto s : sizes_){
+		phi += s;
+	}
+	return phi;
 }
 
 MixPool& MixPool::operator+=(const MixPool & other){
-	size_ += other.size();
+	sizes_[0] += other.size();
 	return *this;
 }
 
 MixPool MixPool::generate(Time, unsigned n){
-	return MixPool(patch_id_,n);
+	return MixPool(patch_id_, n);
 }
 
 MixPool MixPool::generate(Time, const MixPool & other){
 	return other;
 }
 
-MixPool MixPool::sample(RNGcore *, unsigned n) const{
-	n = std::min(size_, n);
-	return MixPool(patch_id_,n);
+void MixPool::shift(RNGcore * rng){
+	if (sizes_.size() > 1){
+		for (int i = sizes_.size()-2; i >= 0; --i){
+			std::binomial_distribution Distr(sizes_[i], rate_);
+			unsigned n = Distr(*rng);
+			sizes_[i+1] += n;
+			sizes_[i] -= n;
+		}
+	}
 }
 
-MixPool MixPool::sampleWithReplacement(RNGcore *, unsigned n) const{
-	return MixPool(patch_id_,n);
+MixPool MixPool::getNewErased(RNGcore * rng) const{
+	std::binomial_distribution Distr(sizes_.back(), rate_);
+	unsigned n = Distr(*rng);
+	return MixPool(patch_id_, n);
+}
+
+MixPool MixPool::sampleInfectors(RNGcore *, unsigned n) const{
+	return MixPool(patch_id_, n);
 }
 
 void MixPool::moveFromTo(MixPool & source, MixPool & target) const{
-	target.size_ += size_;
-	source.size_ -= size_;
+	target.sizes_[0] += sizes_[0];
+	source.sizes_.back() -= sizes_[0];
 }
 
 void MixPool::clear(){
-	size_ = 0;
+	for (auto & s : sizes_){
+		s = 0;
+	}
 }
 
 
