b3506c6b05d039028b1d7a24e7a6137457f7e1e4
dariobaron <barone.dario97@gmail.com>
2024-05-07 11:05:26 +0200

    added small analysis

--- src/quickanalysis.cpp
+++ src/quickanalysis.cpp
@@ -2,6 +2,8 @@
 
 PYBIND11_MODULE(quickanalysis, m){
 
+	PYBIND11_NUMPY_DTYPE(IdDepth, id, depth);
+
     py::module types = py::module::import("TravelAndMutate.types");
 
 	m.def("computeDepths", &computeDepths);
--- src/quickanalysis.hpp
+++ src/quickanalysis.hpp
@@ -1,27 +1,30 @@
 #ifndef QUICKANALYSIS_HPP
 #define QUICKANALYSIS_HPP
 
+#include <map>
 #include "types.hpp"
 #include "haplotypes.hpp"
 
 struct IdDepth{
 	unsigned id, depth;
 };
-
-np_array<IdDepth> computeDepths(const Haplotypes * dealer){
-	auto parents = dealer->getParents();
-	np_array<IdDepth> depths(parents.size());
-	auto view = depths.mutable_unchecked<1>();
-	for (unsigned i = 0; i < parents.size(); ++i){
-		view[i].id = i;
-		if (parents[i] < parents.size()){
-			view[i].depth = view[parents[i]].depth + 1;
-		}
-		else{
-			view[i].depth = 0;
-		}
+np_array<IdDepth> computeDepths(const np_array<ParentChild> & tree){
+	std::map<unsigned,unsigned> depths;
+	auto view_tree = tree.unchecked<1>();
+	for (unsigned i = 0; i < tree.shape(0); ++i){
+		auto it = depths.find(view_tree[i].parent);
+		if (it == depths.end())	{	depths[view_tree[i].child] = 0;	}
+		else					{	depths[view_tree[i].child] = 1 + depths[view_tree[i].parent];	}
 	}
-	return depths;
+	np_array<IdDepth> arr_depths(depths.size());
+	auto view_dept = arr_depths.mutable_unchecked<1>();
+	unsigned idx = 0;
+	for (auto & pair : depths){
+		view_dept[idx].id = pair.first;
+		view_dept[idx].depth = pair.second;
+		++idx;
+	}
+	return arr_depths;
 }
 
 
